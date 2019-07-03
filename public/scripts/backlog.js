92b5c224b23a2bf58574f96826d0aaf0b96b2e0b
issei.kariya <issei@isseikariyanoMacBook-Pro.local>
2019-07-03 11:27:26 +0900

    backlog全てのドメインに対応できるように変更

--- public/scripts/backlog.js
+++ public/scripts/backlog.js
@@ -1,5 +1,5 @@
 /*const getEvent = async (Id, apiKey, workspace) => {
-  const backlog = `https://${workspace}.backlog.com/api/v2`;
+  const backlog = `https://${workspace}/api/v2`;
   let params = {"apiKey": apiKey};
   axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
   params['projectId[]'] = Id;
@@ -9,48 +9,45 @@
 }*/
 
 const getIssue = async (Id, apiKey, workspace) => {
-  const backlog = `https://${workspace}.backlog.com/api/v2/issues`;
-  let params = {"apiKey": apiKey};
-  axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-  return await axios.get(`${backlog}/${Id}?${$.param(params)}`);
+  const backlog = `https://${workspace}/api/v2/issues`
+  let params = { apiKey: apiKey }
+  axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+  return await axios.get(`${backlog}/${Id}?${$.param(params)}`)
 }
 
 const patchIssue = async (Id, status, apiKey, workspace) => {
-  const backlog = `https://${workspace}.backlog.com/api/v2/issues`;
+  const backlog = `https://${workspace}/api/v2/issues`
   let params = {
-    "statusId": status,
-    "apiKey": apiKey
-  };
-  axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-  return await axios.patch(`${backlog}/${Id}?${$.param(params)}`);
+    statusId: status,
+    apiKey: apiKey
+  }
+  axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+  return await axios.patch(`${backlog}/${Id}?${$.param(params)}`)
 }
 
-
-
 const getEvent = async (Id, apiKey, workspace) => {
-  let _length;
-  let records = [];
+  let _length
+  let records = []
 
-  const backlog = `https://${workspace}.backlog.com/api/v2`;
-  let params = {"apiKey": apiKey};
-  axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-  params['projectId[]'] = Id;
-  params['count'] = 100;
-  params['offset'] = 0;
-  const Scroll = async (_len) => {
-    if (_len < 100 || _len === undefined){
-      return records;
+  const backlog = `https://${workspace}/api/v2`
+  let params = { apiKey: apiKey }
+  axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+  params['projectId[]'] = Id
+  params['count'] = 100
+  params['offset'] = 0
+  const Scroll = async _len => {
+    if (_len < 100 || _len === undefined) {
+      return records
     }
     const result = await axios.get(`${backlog}/issues?${$.param(params)}`)
-    if(!records){
+    if (!records) {
       records = result.data[0]
     } else {
-      records.push(...result.data);
+      records.push(...result.data)
     }
-    params['offset'] += 100;
-    _length = (result.data).length;
-    return Scroll(_length);
+    params['offset'] += 100
+    _length = result.data.length
+    return Scroll(_length)
   }
-  return await Scroll(100);
-};
-
+  return await Scroll(100)
+}
--- public/scripts/buttons.js
+++ public/scripts/buttons.js
@@ -1,88 +1,90 @@
-$(function(){
-  $('.back').on('click', function(){
-    const search = parseQuery(window.location.search);
-    if (search.pj|| search.pj !== undefined){
+$(function() {
+  $('.back').on('click', function() {
+    const search = parseQuery(window.location.search)
+    if (search.pj || search.pj !== undefined) {
       window.location.href = `/main.html#${search.pj}`
     } else {
       window.location.href = '/main.html'
     }
   })
-});
+})
 
-$(function(){
-  $('#save-event').on('click', function(){
-    const apiKey = $('#apiKey').val();
-    const workspace = $('#workspace').val();
+$(function() {
+  $('#save-event').on('click', function() {
+    const apiKey = $('#apiKey').val()
+    const workspace = $('#workspace').val()
     const KeyInfo = {
-      "KeyInfo": {
-        "apiKey": apiKey,
-        "workspace": workspace
+      KeyInfo: {
+        apiKey: apiKey,
+        workspace: workspace
       }
-    };
-    chrome.storage.sync.set(KeyInfo, function(){
+    }
+    chrome.storage.sync.set(KeyInfo, function() {
       window.location.href = '/main.html'
     })
   })
 })
 
-$(function(){
-  $('#post-event').on('click', function(){
-    var formData = $("#issueForm").serializeArray();
-    let search = parseQuery(window.location.search);
-    const pj = search.pj;
+$(function() {
+  $('#post-event').on('click', function() {
+    var formData = $('#issueForm').serializeArray()
+    let search = parseQuery(window.location.search)
+    const pj = search.pj
     let params = {
-      'projectId': pj,
-      'summary': '',
-      'description': '',
-      'startDate': '',
-      'dueDate': '',
+      projectId: pj,
+      summary: '',
+      description: '',
+      startDate: '',
+      dueDate: '',
       'categoryId[]': '',
-      'priorityId': '',
-      'assigneeId': '',
-      'issueTypeId': '',
-      'apiKey': ''
-    };
-    $.each(formData, function(i, element) {
-      params[element.name] = element.value;
-    });
-    if(params['categoryId[]'] === ''){
-      delete params['categoryId[]'];
+      priorityId: '',
+      assigneeId: '',
+      issueTypeId: '',
+      apiKey: ''
     }
-    chrome.storage.sync.get("KeyInfo", function(data){
-      const apiKey = data.KeyInfo.apiKey;
-      const workspace = data.KeyInfo.workspace;
-      const backlog = `https://${workspace}.backlog.com/api/v2/issues`;
-      params['apiKey'] = apiKey ;
-      axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+    $.each(formData, function(i, element) {
+      params[element.name] = element.value
+    })
+    if (params['categoryId[]'] === '') {
+      delete params['categoryId[]']
+    }
+    chrome.storage.sync.get('KeyInfo', function(data) {
+      const apiKey = data.KeyInfo.apiKey
+      const workspace = data.KeyInfo.workspace
+      const backlog = `https://${workspace}/api/v2/issues`
+      params['apiKey'] = apiKey
+      axios.defaults.headers['Content-Type'] =
+        'application/x-www-form-urlencoded'
 
-      axios.post(`${backlog}?${$.param(params)}`)
-      .then(res => {
-        window.location.href = `/main.html#${pj}`
-        return false;
-      })
-      .catch(err => {
-        alert(err);
-        return false;
-      })
+      axios
+        .post(`${backlog}?${$.param(params)}`)
+        .then(res => {
+          window.location.href = `/main.html#${pj}`
+          return false
+        })
+        .catch(err => {
+          alert(err)
+          return false
+        })
     })
   })
 })
 
-$(function(){
-  $('#task-done').on('click', function(){
-    const search = parseQuery(window.location.search);
-    chrome.storage.sync.get("KeyInfo", function(data){
-      const apiKey = data.KeyInfo.apiKey;
-      const workspace = data.KeyInfo.workspace;
+$(function() {
+  $('#task-done').on('click', function() {
+    const search = parseQuery(window.location.search)
+    chrome.storage.sync.get('KeyInfo', function(data) {
+      const apiKey = data.KeyInfo.apiKey
+      const workspace = data.KeyInfo.workspace
       patchIssue(search.Id, '4', apiKey, workspace)
-      .then(res => {
-        window.location.href = `/main.html#${search.pj}`
-        //console.log(res)
-      })
-      .catch(err => {
-        console.log(err);
-        alert(err);
-      })
+        .then(res => {
+          window.location.href = `/main.html#${search.pj}`
+          //console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+          alert(err)
+        })
     })
   })
-});
+})
